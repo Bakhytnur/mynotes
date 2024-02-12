2103a884b9cef9f8cb3f67e1590a80528d7ef32a
Bakhytnur <b.s.gabdullin@gmail.com>
2024-02-12 18:03:17 +0600

    commit edit server db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     host: "localhost",
     port: 5000,
     database: "notes",
-    connectionString: "postgresql://postgres:123123@localhost:5000/notes?charset=utf8"
+    //connectionString: "postgresql://postgres:123123@localhost:5000/notes?charset=utf8"
+    client_encoding: "UTF8"
 })
 
 module.exports = pool;
